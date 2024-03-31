7263454f407bc6ecaae3d9708fd4ea1f2daf5e45
Mike-Gilge <mgilge@gmail.com>
2024-03-30 17:05:09 -0700

    working on monorepo

---
+++ org/apps/gtech-industries/src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="gtech-industries" />
+    </StyledApp>
+  );
+}
+
+export default App;
