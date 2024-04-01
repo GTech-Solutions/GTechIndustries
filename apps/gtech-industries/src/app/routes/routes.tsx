123fd191db4c2cb15e049632db95b9e92ab0b94a
Mike-Gilge <mgilge@gmail.com>
2024-04-01 12:10:30 -0700

    bump

--- apps/gtech-industries/src/app/routes/routes.tsx
+++ apps/gtech-industries/src/app/routes/routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { GtechIndustriesLayout } from '../gtech-industries-layout/gtech-industries-layout';
 import { CustomDataGrid, LayoutRoute, dataGridIdentifiers } from '@gtech/shared-components';
-import { Route, Routes as RouterRoutes } from 'react-router-dom';
+import { Navigate, Route, Routes as RouterRoutes } from 'react-router-dom';
 import { routeUrls } from './routeUrls';
 import { AutoSaveDataGridController } from '../auto-save-data-grid-controller/auto-save-data-grid-controller';
 import { ManualSaveDataGridController } from '../manual-save-data-grid-controller/manual-save-data-grid-controller';
@@ -12,9 +12,11 @@ export interface IRoutesProps {}
 const Routes: React.FC<IRoutesProps> = (props) => {
     return (
         <RouterRoutes>
-            <Route path={'/'} element={<LayoutRoute layoutComponent={GtechIndustriesLayout} />}>
+            <Route path={'*'} element={<Navigate to={routeUrls.dataGridAutoSave} />} />
+            <Route path={'*'} element={<LayoutRoute layoutComponent={GtechIndustriesLayout} />}>
                 <Route path={routeUrls.dataGridAutoSave} element={<AutoSaveDataGridController />} />
                 <Route path={routeUrls.dataGridManualSave} element={<ManualSaveDataGridController />} />
+                <Route path={routeUrls.treeView} element={<div>TreeView</div>} />
             </Route>
         </RouterRoutes>
     );
