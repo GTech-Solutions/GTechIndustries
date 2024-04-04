7c7e49f21c322d299d8ae1ecd815fa32313623e2
Mike-Gilge <mgilge@gmail.com>
2024-04-03 20:23:02 -0700

    both datagrids are now fluid

--- apps/gtech-industries/src/app/manual-save-data-grid-controller/manual-save-data-grid-controller.tsx
+++ apps/gtech-industries/src/app/manual-save-data-grid-controller/manual-save-data-grid-controller.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { makeStyles } from 'tss-react/mui';
 import { CustomDataGrid, dataGridIdentifiers } from '@gtech/shared-components';
 import { useDemoData } from '@mui/x-data-grid-generator';
+import { GridColDef, GridRenderCellParams } from '@mui/x-data-grid-pro';
+import { Avatar, Rating } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface IManualSaveDataGridControlllerProps {}
@@ -15,11 +18,83 @@ const ManualSaveDataGridController: React.FC<IManualSaveDataGridControlllerProps
         maxColumns: 6,
     });
 
+    const columns: GridColDef<any>[] = [
+        {
+            field: 'id',
+        },
+        {
+            field: 'avatar',
+            headerName: 'Avatar',
+            display: 'flex',
+            sortable: false,
+            filterable: false,
+            groupable: false,
+            aggregable: false,
+            disableExport: true,
+            renderCell: (params) => {
+                return (
+                    <>
+                        <Avatar sx={{ backgroundColor: params.row.avatar }}>{params.row.name[0]}</Avatar>
+                    </>
+                );
+            },
+        },
+        {
+            field: 'name',
+            headerName: 'Name',
+            flex: 1,
+            editable: true,
+            groupable: false,
+            aggregable: false,
+        },
+        {
+            field: 'website',
+            headerName: 'Website',
+            flex: 1,
+            editable: true,
+            groupable: false,
+            aggregable: false,
+            renderCell: (params: GridRenderCellParams) => {
+                return (
+                    <Link className={cx(classes.linkTextColor)} to={params.value}>
+                        {params.value}
+                    </Link>
+                );
+            },
+        },
+        {
+            field: 'rating',
+            headerName: 'Rating',
+            flex: 1,
+            type: 'number',
+            align: 'left',
+            headerAlign: 'left',
+            editable: true,
+            availableAggregationFunctions: ['avg', 'min', 'max', 'size'],
+            renderCell: (params: GridRenderCellParams) => {
+                return <Rating name='rating' value={params.value} />;
+            },
+        },
+        {
+            field: 'email',
+            headerName: 'Email',
+            flex: 1,
+            renderCell: (params: GridRenderCellParams) => {
+                return (
+                    <a className={cx(classes.linkTextColor)} href={`mailto:${params.value}`}>
+                        {params.value}
+                    </a>
+                );
+            },
+            editable: true,
+        },
+    ];
+
     return (
         <CustomDataGrid
             defaultHiddenColumns={{ id: false }}
             dataGridIdentifier={dataGridIdentifiers.dataGridManualSave}
-            columns={data.columns}
+            columns={columns}
             rows={data.rows}
             loading={loading}
             withManualSaveTableState
@@ -27,6 +102,10 @@ const ManualSaveDataGridController: React.FC<IManualSaveDataGridControlllerProps
     );
 };
 
-const useStyles = makeStyles<IManualSaveDataGridControlllerProps>()((theme, props) => ({}));
+const useStyles = makeStyles<IManualSaveDataGridControlllerProps>()((theme, props) => ({
+    linkTextColor: {
+        color: 'black',
+    },
+}));
 
 export { ManualSaveDataGridController };
