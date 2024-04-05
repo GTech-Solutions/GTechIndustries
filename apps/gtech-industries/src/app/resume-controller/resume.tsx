d07930c3f28b2ba51b80bf0bb0c51fb3a0b8d60a
Mike-Gilge <mgilge@gmail.com>
2024-04-05 16:29:39 -0700

    Update resume.tsx

--- apps/gtech-industries/src/app/resume-controller/resume.tsx
+++ apps/gtech-industries/src/app/resume-controller/resume.tsx
@@ -25,28 +25,91 @@ const Resume = React.forwardRef<HTMLDivElement>((props: IResumeProps, ref) => {
                 }}
                 sections={[
                     {
-                        type: 'text',
-                        title: 'Career Profile',
-                        content: `When I was a child, I always wanted to be a developer..then this hapened **You can use markdown here**`,
-                        icon: 'usertie',
+                        type: 'experiences-list',
+                        title: 'Vega Cloud',
+                        icon: 'archive',
+                        items: [
+                            {
+                                title: 'Principle Architect',
+                                company: 'Vega',
+                                description: "I'm working as a lead developer yeeeey!But wooo",
+                                companyWebSite: 'https://www.vegacloud.io',
+                                companyMeta: '',
+                                datesBetween: '2023.8 - Present',
+                                descriptionTags: ['Javascript', 'React', 'MUI', 'AWS'],
+                            },
+                            {
+                                title: 'Engineering Team Lead',
+                                company: 'Vega',
+                                description: "I'm working as a lead developer yeeeey!But wooo",
+                                companyWebSite: 'https://www.vegacloud.io',
+                                companyMeta: '',
+                                datesBetween: '2022.10 - 2023.8',
+                                descriptionTags: ['Javascript', 'React', 'MUI', 'AWS'],
+                            },
+                            {
+                                title: 'Senior Software Engineer',
+                                company: 'Vega',
+                                description: "I'm working as a lead developer yeeeey!But wooo",
+                                companyWebSite: 'https://www.vegacloud.io',
+                                companyMeta: '',
+                                datesBetween: '2019.10 - 2022.10',
+                                descriptionTags: ['Javascript', 'React', 'MUI', 'AWS'],
+                            },
+                        ],
                     },
                     {
-                        type: 'text',
-                        title: 'Career Profile',
-                        content: `When I was a child, I always wanted to be a developer..then this hapened **You can use markdown here**`,
-                        icon: 'usertie',
+                        type: 'experiences-list',
+                        title: 'RiskLens',
+                        icon: 'archive',
+                        items: [
+                            {
+                                title: 'Software Engineer',
+                                company: 'RiskLens',
+                                description: "I'm working as a lead developer yeeeey!But wooo",
+                                companyWebSite: 'https://www.vegacloud.io',
+                                companyMeta: '',
+                                datesBetween: '22019.5 - 2019.9',
+                                descriptionTags: ['Javascript', 'React', 'MUI', 'AWS'],
+                            },
+                            {
+                                title: 'Junior Software Engineer',
+                                company: 'RiskLens',
+                                description: "I'm working as a lead developer yeeeey!But wooo",
+                                companyWebSite: 'https://www.vegacloud.io',
+                                companyMeta: '',
+                                datesBetween: '2018.5 - 2019.5',
+                                descriptionTags: ['Javascript', 'React', 'MUI', 'AWS'],
+                            },
+                            {
+                                title: 'Intern',
+                                company: 'RiskLens',
+                                description: "I'm working as a lead developer yeeeey!But wooo",
+                                companyWebSite: 'https://www.vegacloud.io',
+                                companyMeta: '',
+                                datesBetween: '2018.2 - 2018.5',
+                                descriptionTags: ['Javascript', 'React', 'MUI', 'AWS'],
+                            },
+                        ],
                     },
                     {
-                        type: 'text',
-                        title: 'Career Profile',
-                        content: `When I was a child, I always wanted to be a developer..then this hapened **You can use markdown here**`,
-                        icon: 'usertie',
-                    },
-                    {
-                        type: 'text',
-                        title: 'Career Profile',
-                        content: `When I was a child, I always wanted to be a developer..then this hapened **You can use markdown here**`,
-                        icon: 'usertie',
+                        type: 'common-list',
+                        title: 'Education',
+                        icon: 'graduation',
+                        items: [
+                            {
+                                title: 'Computer Engineering (Bachelor)',
+                                authority: 'Eastern Washington University',
+                                authorityWebSite: 'https://www.ewu.edu/',
+                                rightSide: '2014 - 2018',
+                            },
+                            {
+                                title: 'Some Department (PHD)',
+                                authority: 'Another University',
+                                authorityWebSite: 'https://sample.edu',
+                                rightSide: '2017 - Present',
+                            },
+                        ],
                     },
                 ]}
                 branding={false} // or false to hide it.
@@ -57,11 +120,12 @@ const Resume = React.forwardRef<HTMLDivElement>((props: IResumeProps, ref) => {
 
 const useStyles = makeStyles<IResumeProps>()((theme, props) => ({
     resume: {
+        minWidth: '100vw',
         overflow: 'scroll',
         '& .EIrz6a_hero.EIrz6a_is-primary': {
             backgroundColor: theme.palette.primary.main,
         },
-        '& .EIrz6a_container': { maxHeight: 'calc(100vh - 14rem)' },
+
         '&. .SPgITW_appContainer': {
             paddingTop: 0,
         },
