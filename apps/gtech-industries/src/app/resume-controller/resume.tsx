99b63ffd00bf4c77ab2a8e86cbad4ef6185f4b2d
Mike-Gilge <mgilge@gmail.com>
2024-04-05 17:50:44 -0700

    Update resume.tsx

--- apps/gtech-industries/src/app/resume-controller/resume.tsx
+++ apps/gtech-industries/src/app/resume-controller/resume.tsx
@@ -25,18 +25,25 @@ const Resume = React.forwardRef<HTMLDivElement>((props: IResumeProps, ref) => {
                 }}
                 sections={[
                     {
+                        type: 'text',
+                        title: 'Career Profile',
+                        content: `I enjoy working with and mentoring people on all aspects of software engineering especially front end and CICD pipelines. Bringing a history of more than 10 years' involvement in technology -- from support to principal software architect -- I've learned how to absorb information quickly and how to convey that information to audiences at various skill levels.  My former background as an automotive technician gives me a lot of experience with problem solving and thinking outside the box that I enjoy being able to share with others.`,
+                        icon: 'usertie',
+                    },
+                    {
                         type: 'experiences-list',
-                        title: 'Vega Cloud',
+                        title: 'Experience',
                         icon: 'archive',
                         items: [
                             {
                                 title: 'Principle Architect',
                                 company: 'Vega',
-                                description: "I'm working as a lead developer yeeeey!But wooo",
+                                description:
+                                    'Support cross-functional teams by delivering cloud and software architecture services with an emphasis on delivery of our FinOps Saas platform. Provide guidance, mentorship, training, documentation, and cloud expertise on an as-needed basis. Collaborate with stakeholders in all departments to ensure our platform delivers on their needs.',
                                 companyWebSite: 'https://www.vegacloud.io',
                                 companyMeta: '',
                                 datesBetween: '2023.8 - Present',
-                                descriptionTags: ['Javascript', 'React', 'MUI', 'AWS'],
+                                descriptionTags: ['Terraform', 'CDK', 'CFT', 'AWS', 'Azure'],
                             },
                             {
                                 title: 'Engineering Team Lead',
@@ -56,20 +63,13 @@ const Resume = React.forwardRef<HTMLDivElement>((props: IResumeProps, ref) => {
                                 datesBetween: '2019.10 - 2022.10',
                                 descriptionTags: ['Javascript', 'React', 'MUI', 'AWS'],
                             },
-                        ],
-                    },
-                    {
-                        type: 'experiences-list',
-                        title: 'RiskLens',
-                        icon: 'archive',
-                        items: [
                             {
                                 title: 'Software Engineer',
                                 company: 'RiskLens',
                                 description: "I'm working as a lead developer yeeeey!But wooo",
                                 companyWebSite: 'https://www.vegacloud.io',
                                 companyMeta: '',
-                                datesBetween: '22019.5 - 2019.9',
+                                datesBetween: '2019.5 - 2019.9',
                                 descriptionTags: ['Javascript', 'React', 'MUI', 'AWS'],
                             },
                             {
@@ -78,38 +78,53 @@ const Resume = React.forwardRef<HTMLDivElement>((props: IResumeProps, ref) => {
                                 description: "I'm working as a lead developer yeeeey!But wooo",
                                 companyWebSite: 'https://www.vegacloud.io',
                                 companyMeta: '',
-                                datesBetween: '2018.5 - 2019.5',
-                                descriptionTags: ['Javascript', 'React', 'MUI', 'AWS'],
-                            },
-                            {
-                                title: 'Intern',
-                                company: 'RiskLens',
-                                description: "I'm working as a lead developer yeeeey!But wooo",
-                                companyWebSite: 'https://www.vegacloud.io',
-                                companyMeta: '',
-                                datesBetween: '2018.2 - 2018.5',
+                                datesBetween: '2018.2 - 2019.5',
                                 descriptionTags: ['Javascript', 'React', 'MUI', 'AWS'],
                             },
                         ],
                     },
+
                     {
                         type: 'common-list',
                         title: 'Education',
                         icon: 'graduation',
                         items: [
                             {
-                                title: 'Computer Engineering (Bachelor)',
+                                title: 'Computer Science (Bachelor)',
                                 authority: 'Eastern Washington University',
                                 authorityWebSite: 'https://www.ewu.edu/',
                                 rightSide: '2014 - 2018',
                             },
                             {
-                                title: 'Some Department (PHD)',
-                                authority: 'Another University',
-                                authorityWebSite: 'https://sample.edu',
-                                rightSide: '2017 - Present',
+                                title: 'Automotive Technology (Associate)',
+                                authority: 'Lewis Clark State College',
+                                authorityWebSite: 'https://lcsc.edu',
+                                rightSide: '2004 - 2006',
                             },
                         ],
+                    },
+                    {
+                        type: 'common-list',
+                        title: 'Conferences & Certificates',
+                        description: '',
+                        icon: 'comments',
+                        items: [
+                            {
+                                title: 're:Invent 2019',
+                                authority: 'AWS',
+                                authorityWebSite: 'https://reinvent.awsevents.com/',
+                            },
+                            {
+                                title: 'FinOps Certified Practitioner',
+                                authorityMeta: 'https://www.credly.com/badges/a07f13b0-3f69-4243-93c3-b47a2ba770e4/linked_in_profile',
+                            },
+                        ],
+                    },
+                    {
+                        type: 'tag-list',
+                        title: 'Hobbies & Interests',
+                        icon: 'cubes',
+                        items: ['Photography', 'Jeeps', 'Camping', 'Hiking', 'Fishing'],
                     },
                 ]}
                 branding={false} // or false to hide it.
