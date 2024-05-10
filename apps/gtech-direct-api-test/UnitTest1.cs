78e644099d539e49517909712fb093b9ce3088cf
Mike-Gilge <mgilge@gmail.com>
2024-05-10 09:24:37 -0700

    add initial C# api

---
+++ apps/gtech-direct-api-test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Gtech.Apps.GtechDirectApi.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
