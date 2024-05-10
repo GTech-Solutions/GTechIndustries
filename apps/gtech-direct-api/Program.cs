4c39d10112fc2a075bd6925d7b245e63e5c2151e
Mike-Gilge <mgilge@gmail.com>
2024-05-10 14:54:04 -0700

    docker file works

--- apps/gtech-direct-api/Program.cs
+++ apps/gtech-direct-api/Program.cs
@@ -1,6 +1,34 @@
+var  CorsAllowSpecificOrigins = "_corsAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+ConfigureServices();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
 
+
+app.UseCors(CorsAllowSpecificOrigins);
+app.MapControllers();
+app.MapHealthChecks("/");
+
+app.MapGet("/", () => "Hello World!");
 app.Run();
+
+void ConfigureServices()
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddControllers();
+    builder.Services.AddHealthChecks();
+
+    builder.Services.AddCors(options =>
+    {
+      options.AddPolicy(name: CorsAllowSpecificOrigins,
+        policy  =>
+        {
+          policy.WithOrigins("*");
+          policy.AllowAnyHeader();
+        });
+    });
+}
